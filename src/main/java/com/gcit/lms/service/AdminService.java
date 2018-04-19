392e255428e2896f7b6b51f8c448fdc59c2f4669
Soe Lynn Htike <ship.gorgon08@gmail.com>
2018-04-18 22:42:03 -0400

    tset

--- src/main/java/com/gcit/lms/service/AdminService.java
+++ src/main/java/com/gcit/lms/service/AdminService.java
@@ -195,7 +195,7 @@ public class AdminService extends BaseController {
 
 		try {
 				adao.deleteAuthorBook(author);
-				System.out.print("sda");
+				System.out.print("sdaaa");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace(); // log your stacktrace
